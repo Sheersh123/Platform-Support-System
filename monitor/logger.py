f580a08a6dab96a6a8db09716598967e0dca749b
Sheersh <sheershsinha30@gmail.com>
2026-04-09 13:16:54 +0530

    Updated-files

--- monitor/logger.py
+++ monitor/logger.py
@@ -1,12 +1,22 @@
+import os
 import json
-import datetime
-LOG_FILE = "../logs/logs.json"
+from datetime import datetime
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+LOG_FILE = os.path.join(LOG_DIR, "logs.json")
+os.makedirs(LOG_DIR, exist_ok=True)
+if not os.path.exists(LOG_FILE):
+    with open(LOG_FILE, "w") as f:
+        json.dump([], f)
 def log_event(service, level, message):
-    log = {
-        "timestamp": str(datetime.datetime.now()),
+    log_entry = {
+        "timestamp": str(datetime.now()),
         "service": service,
         "level": level,
         "message": message
     }
-    with open(LOG_FILE, "a") as f:
-        f.write(json.dumps(log) + "\n")
+    with open(LOG_FILE, "r+") as f:
+        data = json.load(f)
+        data.append(log_entry)
+        f.seek(0)
+        json.dump(data, f, indent=2)
